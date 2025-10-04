9d30b0659d7bd3d7da399b27a94f2e524e6a97e3
Narendra <narendragatram8533@gmail.com>
2025-10-04 13:45:47 +0530

    feat(easy): adding the question statement in the inverted right half pyramid pattern using stars

--- inverted_right_half_pyramid.py
+++ inverted_right_half_pyramid.py
@@ -1,4 +1,16 @@
-n = int(input('Enter no of rows : '))
+'''
+    Pattern : Inverted Right Half Pyramid
+    Input : 5
+    Output :
+    * * * * *
+    * * * * 
+    * * *
+    * *
+    *
+'''
+
+
+n = int(input('Enter no of rows : '))# read number of rows from user
 for i in range(1,n+1):
     for j in range(n-i+1):
         print('*',end=' ')
