bae085fe973d33d463e8ebd7fba515a6a35d7bc3
Narendra <narendragatram8533@gmail.com>
2025-10-05 23:34:31 +0530

    feat(easy):solved the inverted left half pyramid pattern

--- inverted_lefthalf_pyramid.py
+++ inverted_lefthalf_pyramid.py
@@ -1,9 +1,24 @@
+'''
+    Pattern  : Inverted Left Half Pyramid
+    Input: ex-> 6
+    o/p:
+    * * * * * *
+      * * * * *
+        * * * *
+          * * *
+            * *
+              *
+'''
+
+
 n = int(input('Enter no of rows : '))
-for i in range(n):
-    for j in range(n,0,-1):
-        if j<=i:
-            print(' ',end=' ')
-        else:
-            print('*',end=' ')
+for i in range(1,n+1):
+    
+    for j in range(1,i):
+        print(' ',end=' ')
+    
+    for j in range(n-i+1):
+        print('*',end=' ')
+ 
     print()
     
