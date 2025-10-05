35e2374af25b6268097e6fa1cb6f01ed681b5bf2
Narendra <narendragatram8533@gmail.com>
2025-10-05 23:52:35 +0530

    feat(easy):solved the left half pyramid pattern

---
+++ left_half_pyramid.py
@@ -0,0 +1,18 @@
+'''
+    Pattern  : Left Half Pyramid
+    Input: ex-> 5
+    o/p:
+            * 
+          * *
+        * * *
+      * * * *
+    * * * * *           
+'''
+
+n = int(input('Enter no of rows : '))
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(' ',end=' ')
+    for j in range(i):
+        print('*',end=' ')
+    print()
